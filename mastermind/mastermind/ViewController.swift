bf09fd8876436558fad4b532b66017d6ab9520cb
Thomas Millet <tm.lepoleswebforce3@gmail.com>
2018-11-27 11:28:48 +0100

    init

---
+++ mastermind/mastermind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mastermind
+//
+//  Created by Thomas Millet on 27/11/2018.
+//  Copyright © 2018 Thomas Millet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
