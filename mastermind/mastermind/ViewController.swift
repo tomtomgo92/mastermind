bb0a4901441421a71194a6fa36ee3f30a9ef0d64
Grandpierre Adrien <adrigrand@yahoo.fr>
2018-11-27 15:21:47 +0100

    from Adrien

--- mastermind/mastermind/ViewController.swift
+++ mastermind/mastermind/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let number = String(Int.random(in: 1000..<10000))
+    
+    @IBOutlet weak var NumberField0:  UITextField!
+    @IBOutlet weak var NumberField1: UITextField!
+    @IBOutlet weak var NumberField2: UITextField!
+    @IBOutlet weak var NumberField3: UITextField!
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func BtnClicked(_ sender: Any) {
+        
+        
+        for i in 0..<number.count{
+
+            let characters = Array(number)
+            print(characters[i])
+            print(i)
+            
+            if(String(characters[i]) == "\(NumberField)\(i)".text!){
+                print("Rouge")
+            }
+        }
+        
+        
+        
+        let userCode =
+                NumberField0.text! +
+                NumberField1.text! +
+                NumberField2.text! +
+                NumberField3.text!
+        
+        print(number)
+        print(userCode)
+        
+        
+    }
+    
 
 }
 
